146a4e67cd921c12b1a8871df99c39d12eee512a
marc <marc@lamarciana.com>
2013-04-09 13:42:30 +0200

    Add block argument to query method

--- lib/panlex_client/panlex_client.rb
+++ lib/panlex_client/panlex_client.rb
@@ -1,7 +1,7 @@
 module PanlexClient
    API_URL = 'http://api.panlex.org/'
 
-   def self.query(param, body)
-      RestClient.post API_URL+param, body.to_json, :content_type => :json, :accept => :json
+   def self.query(param, body, &block)
+      RestClient.post API_URL+param, body.to_json, { :content_type => :json, :accept => :json }, &block
    end
 end
